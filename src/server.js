f41b0c2150aaee5bbe1a437db9749b62e7c7238c
2theMoon <dlaguddh1@gmail.com>
2021-08-04 15:06:42 +0900

    Update morgan

--- src/server.js
+++ src/server.js
@@ -1,25 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
-const PORT = 4000;
+const PORT = 4000;  // 포트번호 4000사용 백엔드 관습
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+const home = (req, res) => {
+  return res.end("hello");  
+};
+const login = (req, res) => {
+  return res.send("login");
 }
 
-
-
-const handleHome = (req, res, next) => {
-  return res.end("I love middlewares");  
-};
-
-app.get("/" , logger, handleHome);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 
 const handleListening = () => 
     console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
-
+app.listen(PORT, handleListening);  //서버 리스닝중
