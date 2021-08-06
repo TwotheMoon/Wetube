fc72a100c304933d001070cbb23fc39685c353bd
2theMoon <dlaguddh1@gmail.com>
2021-08-06 22:48:15 +0900

    Update Routers

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
-import express from "express";
-import {edit, remove} from "../controllers/userController"
+import express from "express";  // node_modules 에서 임포트 (패키지)
+import {edit, remove, logout, see} from "../controllers/userController" // 내가 만든 폴더에서 함수 임포트
 
 const userRouter = express.Router();
 
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
+userRouter.get("/remove", remove);
+userRouter.get(":id", see);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
-import express from "express";
-import {watch, edit} from "../controllers/videoController";
+import express from "express"; // node_modules 에서 임포트 (패키지)
+import {see, edit, upload, deleteVideo} from "../controllers/videoController";// 내가 만든 폴더에서 함수 임포트
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
