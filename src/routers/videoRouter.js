2157eda878f4f5a96edf5dbf3d91d5c0f48ee3fd
2theMoon <dlaguddh1@gmail.com>
2021-08-18 12:50:36 +0900

    Edit video part

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express"; // node_modules 에서 임포트 (패키지)
-import {see, edit, upload, deleteVideo} from "../controllers/videoController";// 내가 만든 폴더에서 함수 임포트
+import {
+    watch, upload, deleteVideo, getEdit, postEdit,
+} from "../controllers/videoController";// 내가 만든 폴더에서 함수 임포트
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -5,13 +5,13 @@ import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
 const PORT = 4000;  // 포트번호 4000사용 백엔드 관습
-
 const app = express();
 const logger = morgan("dev");
  
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-app.use(logger);;
+app.use(logger);
+app.use(express.urlencoded({extended:true}));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
