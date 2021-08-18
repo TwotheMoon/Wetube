6d68133292ca3a786a2b918a3b3b0b75f4519504
2theMoon <dlaguddh1@gmail.com>
2021-08-18 17:34:42 +0900

    Update get, post logic

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,17 @@
 import express from "express"; // node_modules 에서 임포트 (패키지)
 import {
-    watch, upload, deleteVideo, getEdit, postEdit,
+    watch, 
+    getUpload, 
+    postUpload,
+    getEdit, 
+    postEdit,
 } from "../controllers/videoController";// 내가 만든 폴더에서 함수 임포트
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+
 
 export default videoRouter;
