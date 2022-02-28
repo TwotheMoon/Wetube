256b46d33ad5761ff10b87663d1a8e5f50616f78
2theMoon <dlaguddh1@gmail.com>
2022-02-28 20:26:07 +0900

    mixin
    - videos 객체데이터를 받아와
    each in 으로 mixin된 video 컴포넌트에 데이터를 보내서 각각 다른 데이터를 가진 같은 포맷의 컴포넌트 구축

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
 export const trending = (req, res) => {
-    const videos = [];
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 1
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 2
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes age",
+            views: 59,
+            id: 3
+        },
+    ];
     return res.render("home", { pageTitle: "Home", videos }); // home.pug 랜더} 
 }
 export const see = (req, res) => { return res.render("watch", { pageTitle: "Watch" }); }
