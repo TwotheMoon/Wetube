af8ab3160b66713a97254b8f6952ac80a1bbbd92
2theMoon <dlaguddh1@gmail.com>
2022-02-28 17:41:57 +0900

    추가 라우터 정의

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,18 @@
 export const join = (req, res) => {
-    res.send("join");
+    res.send("join User");
 }
 export const edit = (req, res) => {
     res.send("edit User");
 }
 export const remove = (req, res) => {
     res.send("remove User");
+}
+export const login = (req, res) => {
+    res.send("login User");
+}
+export const logout = (req, res) => {
+    res.send("log out User");
+}
+export const see = (req, res) => {
+    res.send("See User");
 }
