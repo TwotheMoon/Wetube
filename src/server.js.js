772cdc9ddd5ee32637f5a27cd70cefc4141097e5
2theMoon <dlaguddh1@gmail.com>
2022-02-28 12:54:50 +0900

    request, response
    콜백 함수를 이용해 express에서 제공해주는 res,req 객체를 받아 제공하는 메소드를 이용해 response 해보기

--- src/server.js.js
+++ src/server.js.js
@@ -1,9 +1,22 @@
 import express from "express";
 
 const PORT = 4000;
-
+// 서버 구축 선언
 const app = express();
 
+
+// 서버 내부 로직
+const handleHome = (req, res) => {
+    return res.send("<h1>sdf</h1>");
+}
+const handleLogin = (req, res) => {
+    return res.send("login");
+}
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+
+// 서버 실행하기 (리스닝 실행 )
 const handleListening = () => console.log(`Server Listen on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
