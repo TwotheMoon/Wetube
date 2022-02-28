c95345926bbe0cdcb1020f33d860c1b3a6817b4e
2theMoon <dlaguddh1@gmail.com>
2022-02-28 20:04:32 +0900

    배열 데이터 출력하는법
    each in 문법 pug에서 사용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,7 @@
-export const trending = (req, res) => { res.render("home", { pageTitle: "Home" }); } // home.pug 랜더}
+export const trending = (req, res) => {
+    const videos = [];
+    return res.render("home", { pageTitle: "Home", videos }); // home.pug 랜더} 
+}
 export const see = (req, res) => { return res.render("watch", { pageTitle: "Watch" }); }
 export const edit = (req, res) => { return res.render("edit", { pageTitle: "Edit" }); }
 export const search = (req, res) => {
