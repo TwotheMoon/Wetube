cb59cbfeac064794ae35affb1fbe43bb143faba9
2theMoon <dlaguddh1@gmail.com>
2022-02-28 15:09:17 +0900

    middleware 개념2

--- src/server.js.js
+++ src/server.js.js
@@ -4,17 +4,17 @@ const PORT = 4000;
 // 서버 구축 선언
 const app = express();
 
-
 // 서버 내부 로직
-const handleHome = (req, res) => {
-    return res.send("<h1>sdf</h1>");
-}
-const handleLogin = (req, res) => {
-    return res.send("login");
-}
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
 
+const handleHome = (req, res) => {
+    return res.send("Home");
+};
+
+app.get("/", logger, handleHome);
 
 // 서버 실행하기 (리스닝 실행 )
 const handleListening = () => console.log(`Server Listen on port http://localhost:${PORT}`);
