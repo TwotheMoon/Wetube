4553dc0b3449c487418f869bf9718d1bbbabe286
2theMoon <dlaguddh1@gmail.com>
2022-03-01 16:37:01 +0900

    create 구현

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-    try {
-        const videos = await Video.find({});
-        return res.render("home", { pageTitle: "Home", videos });
-    } catch {
-        return res.render("server-error");
-    }
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
     const { id } = req.params;
@@ -24,7 +20,17 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-    const { title } = req.body;
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    await Video.create({
+        title,
+        description,
+        createdAt: Date.now(),
+        hashtags: hashtags.split(",").map((word) => !word.trim().startsWith("#") ? `#${word.trim()}` : word.trim()),
+        meta: {
+            views: 0,
+            rating: 0,
+        }
+    });
     return res.redirect("/");
 };
