7454292e525e9fa959c1f07437caa7b9ee75c842
2theMoon <dlaguddh1@gmail.com>
2022-03-01 15:21:20 +0900

    콜백 함수로 접근하기

---
+++ src/init.js
@@ -0,0 +1,9 @@
+import "./db";
+import "./models/Video";
+import app from "./server.js";
+
+// 서버 구축 선언
+const PORT = 4000;
+// 서버 실행하기 (리스닝 실행 )
+const handleListening = () => console.log(`Server Listen on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening);
--- src/server.js.js
+++ src/server.js.js
@@ -1,13 +1,9 @@
-import "./db";
-import "./models/Video";
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-// 서버 구축 선언
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -20,6 +16,5 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-// 서버 실행하기 (리스닝 실행 )
-const handleListening = () => console.log(`Server Listen on port http://localhost:${PORT}`);
-app.listen(PORT, handleListening);
+export default app;
+
