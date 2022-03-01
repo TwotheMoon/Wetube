5ac27c817c95105b8f993409b2b5b7dcdbf8039e
2theMoon <dlaguddh1@gmail.com>
2022-03-01 17:56:22 +0900

    edit video 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,13 +4,21 @@ export const home = async (req, res) => {
     const videos = await Video.find({});
     return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const { id } = req.params;
-    return res.render("watch", { pageTitle: `Watching:` });
+    const video = await Video.findById(id);
+    if (video === null) {
+        return res.render("404", { pageTitle: "Video not Found" });
+    }
+    return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing:` });
+    const video = await Video.findById(id);
+    if (video === null) {
+        return res.render("404", { pageTitle: "Video not Found" });
+    }
+    return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
@@ -23,7 +31,6 @@ export const getUpload = (req, res) => {
 export const postUpload = async (req, res) => {
     const { title, description, hashtags } = req.body;
     try {
-
         await Video.create({
             title,
             description,
