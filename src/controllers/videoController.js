c545f248ce503bafc58832bab2a86be78894f21f
2theMoon <dlaguddh1@gmail.com>
2022-03-02 10:10:16 +0900

    find 와 정규식으로 검색처리
    - req.query 로 url title 가져오기
    - Video.find({ 필터 }) 필터 안에 정규식으로 title검색 로직 처리

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 
 // 홈 뷰 read
 export const home = async (req, res) => {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -70,4 +70,18 @@ export const deleteVideo = async (req, res) => {
     const { id } = req.params;
     await Video.findByIdAndDelete(id);
     return res.redirect("/");
-}
+};
+
+// 검색 get
+export const search = async (req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+    if (keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i")
+            },
+        });
+    }
+    return res.render("search", { pageTitle: "Search", videos });
+};
